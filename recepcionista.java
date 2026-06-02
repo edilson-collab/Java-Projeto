276760375b32744ce49c89f8f23020f8c61d65db
edilson-collab <ens6@discente.ifpe.edu.br>
2026-06-02 20:57:06 -0300

    Update recepcionista.java

--- recepcionista.java
+++ recepcionista.java
@@ -3,7 +3,6 @@ public class Recepcionista extends Funcionario {
     private int idRecepcionista;
     private String turno;
 
-    // Construtor
     public Recepcionista(int idRecepcionista, String turno,
                          String numeroConta, int experiencia, String agencia,
                          String nome, String cpf, String email,
@@ -16,7 +15,6 @@ public class Recepcionista extends Funcionario {
         this.turno = turno;
     }
 
-    // Getters e Setters
     public int getIdRecepcionista() {
         return idRecepcionista;
     }
